c5acd8c22689f614b105b8ce8a6fbd9350ee906b
aebebeb <2019038113@chungbuk.ac.kr>
2023-05-16 13:18:02 +0900

    upload card xml file

--- app/src/main/java/com/example/businesscarapp/activity/Galleryactivity.java
+++ app/src/main/java/com/example/businesscarapp/activity/Galleryactivity.java
@@ -8,6 +8,6 @@ public class Galleryactivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.item_gallery);
+        setContentView(R.layout.activity_gallery);
     }
 }
